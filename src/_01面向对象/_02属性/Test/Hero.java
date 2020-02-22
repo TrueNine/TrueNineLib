1bf9606907289c4dc238fa9758c7f49b7395c46a
TrueNine <2394997288@qq.com>
2020-02-22 10:40:26 +0800

    规范了笔记文件的名称,将笔记文件编号为 99_ 格式
    Signed-off-by: TrueNine <2394997288@qq.com>

---
+++ src/_01面向对象/_02属性/Test/Hero.java
@@ -0,0 +1,42 @@
+package _01面向对象._02属性.Test;
+
+/**
+ * 类作用: 英雄类
+ * 类说明: 练习 英雄的属性
+ *
+ * @author 彭继工
+ * @date 2020 - 02 - 22:10:23
+ */
+public class Hero {
+    /** 英雄名称 */
+    String name;
+    /** 英雄血量 */
+    float hp;
+    /** 英雄护甲 */
+    float armor;
+    /** 移动速度 */
+    int moveSpeed;
+    /** 攻击速度 */
+    float attackSpeed;
+    /** 杀人次数 */
+    short k;
+    /** 死亡次数 */
+    short d;
+    /** 助攻次数 */
+    short a;
+    /** 补刀次数 */
+    int killCreatures;
+    /** 杀人前说的话 */
+    String lillBefore;
+    /** 杀人后说的话 */
+    String killAfter;
+
+    /**
+     * 一个空的主方法
+     *
+     * @param args 主方法参数
+     */
+    public static void main(String[] args) {
+
+    }
+}
