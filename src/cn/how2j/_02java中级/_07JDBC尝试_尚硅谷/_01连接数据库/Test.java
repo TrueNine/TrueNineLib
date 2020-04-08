536ce9e595ef1f18f68752b7fb6b81d969d652e2
truenine <2394997288@qq.com>
2020-04-08 21:26:42 +0800

    提交README文件

---
+++ src/cn/how2j/_02java中级/_07JDBC尝试_尚硅谷/_01连接数据库/Test.java
@@ -0,0 +1,15 @@
+package cn.how2j._02java中级._07JDBC尝试_尚硅谷._01连接数据库;
+
+/**
+ * 测试连接数据库
+ *
+ * @author TrueNine
+ * @version 1.0
+ * @time 2020/4/8
+ */
+public class Test {
+    @org.junit.Test
+    public void test() {
+        System.out.println("hello world");
+    }
+}
